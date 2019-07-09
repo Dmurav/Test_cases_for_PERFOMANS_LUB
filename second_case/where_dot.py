4e5b9dbf9477e1fd2f2b97ab9c70e602a826a365
Dima <demmy@bk.ru>
2019-07-09 11:55:58 +0300

    Исправлено решение в задаче №2 в части проверки на вхождение точки

--- second_case/where_dot.py
+++ second_case/where_dot.py
@@ -8,23 +8,34 @@ def where_dot(coordinates, dot):
     dx, dy = d[0], d[1]
     x, y = dot[0], dot[1]
 
-    if x == coordinates[0][0] and y == coordinates[0][1] \
-            or x == coordinates[1][0] and y == coordinates[1][1] \
-            or x == coordinates[2][0] and y == coordinates[2][1] \
-            or x == coordinates[3][0] and y == coordinates[3][1]:
+    vertex_cond = (x == coordinates[0][0] and y == coordinates[0][1]
+                   or x == coordinates[1][0] and y == coordinates[1][1]
+                   or x == coordinates[2][0] and y == coordinates[2][1]
+                   or x == coordinates[3][0] and y == coordinates[3][1])
+    if vertex_cond:
         print('точка - вершина четырехугольника')
-    elif (y - ay) * (bx - ax) == (x - ax) * (by - ay) or \
-            (y - by) * (cx - bx) == (x - bx) * (cy - by) or \
-            (y - cy) * (dx - cx) == (x - cx) * (dy - cy) or \
-            (y - dy) * (cx - dx) == (x - dx) * (ay - dy):
+        return
+
+    side_cond = ((y - ay) * (bx - ax) == (x - ax) * (by - ay) or
+                 (y - by) * (cx - bx) == (x - bx) * (cy - by) or
+                 (y - cy) * (dx - cx) == (x - cx) * (dy - cy) or
+                 (y - dy) * (cx - dx) == (x - dx) * (ay - dy))
+    if side_cond:
         print('точка лежит на сторонах четырехугольника')
-    elif (x - ax) * (x - bx) <= 0 and (y - ay) * (y - by) <= 0 \
-            or (x - bx) * (x - cx) <= 0 and(y - by) * (y - cy) <= 0 \
-            or (x - cx) * (x - dx) <= 0 and (y - cy) * (y - dy) <= 0 \
-            or (x - dx) * (x - ax) <= 0 and (y - dy) * (y - ay) <= 0:
+        return
+
+    in_cond = (
+        ((bx - ax) * (y - ay) < (by - ay) * (x - ax)) and
+        ((cx - bx) * (y - by) < (cy - by) * (x - bx)) and
+        ((dx - cx) * (y - cy) < (dy - cy) * (x - cx)) and
+        ((ax - dx) * (y - dy) < (ay - dy) * (x - dx))
+    )
+
+    if in_cond:
         print('точка внутри четырехугольника')
-    else:
-        print('точка снаружи четырехугольника')
+        return
+
+    print('точка снаружи четырехугольника')
 
 
 if __name__ == '__main__':
